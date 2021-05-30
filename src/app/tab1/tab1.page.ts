389744b8731dad54cd9bae4e6e3cb2ccc1105f9c
Antonio Aparecido Ribeiro <antonio.ribeiro@etec.sp.gov.br>
2021-05-30 20:04:02 -0300

    Adicionei array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -5,11 +6,59 @@ import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController, public toastController: ToastController) {}
+  titulo = 'Vídeos App';
+
+  listaVideos: IFilme[] =  [
+    {
+      nome: 'Sem Remorso (2021)',
+      lancamento: '30/04/2021',
+      duracao: '1h 50m',
+      classificacao: 73,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg',
+      generos: ['Ação', 'Aventura', 'Thriller', 'Guerra']
+    },
+    {
+      nome: 'Vanquish (2021)',
+      lancamento: '16/04/2021',
+      duracao: '1h 36m',
+      classificacao: 62,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1jkNM1NHyHsxiBBuOlOeqgmidM0.jpg',
+      generos: ['Ação', 'Crime', 'Thriller']
+    },
+    {
+      nome: 'Rogai Por Nós (2021)',
+      lancamento: '02/04/2021',
+      duracao: '1h 39m',
+      classificacao: 56,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zduyzcjyahZtP4fGZE6789h04IM.jpg',
+      generos: ['Terror']
+    },
+    {
+      nome: '',
+      lancamento: '',
+      duracao: '',
+      classificacao: 0,
+      cartaz: '',
+      generos: []
+    },
+    {
+      nome: '',
+      lancamento: '',
+      duracao: '',
+      classificacao: 0,
+      cartaz: '',
+      generos: []
+    }
+  ];
+
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController
+  ) {}
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -22,14 +71,15 @@ export class Tab1Page {
           cssClass: 'secondary',
           handler: (blah) => {
             console.log('Confirm Cancel: blah');
-          }
-        }, {
+          },
+        },
+        {
           text: 'SIM, favoritar!',
           handler: () => {
             this.apresentarToast();
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
 
     await alert.present();
@@ -39,10 +89,8 @@ export class Tab1Page {
     const toast = await this.toastController.create({
       message: 'Filme adicionado aos favoritos.',
       duration: 2000,
-      color: 'medium'
-
+      color: 'medium',
     });
     toast.present();
   }
-
 }
